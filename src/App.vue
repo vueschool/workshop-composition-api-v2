e82582b23c3893f219562d5bd25f38639fefd3af
Daniel Kelly <me@danielkelly.io>
2022-12-19 17:16:06 -0600

    3-end

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,17 @@
-<script>
+<script setup>
 import { ref, computed } from "vue";
 import ProductCard from "./components/ProductCard.vue";
-export default {
-  components: { ProductCard },
-  setup() {
-    const loading = ref(true);
-    const products = ref([]);
-    const numberOfProducts = computed(() => products.value.length);
-    async function fetchProducts() {
-      const res = await fetch("https://dummyjson.com/products?limit=10000");
-      const data = await res.json();
-      products.value = data.products;
-    }
-    fetchProducts().then(() => {
-      loading.value = false;
-    });
-    return { loading, products, numberOfProducts, fetchProducts };
-  },
-};
+const loading = ref(true);
+const products = ref([]);
+const numberOfProducts = computed(() => products.value.length);
+async function fetchProducts() {
+  const res = await fetch("https://dummyjson.com/products?limit=10000");
+  const data = await res.json();
+  products.value = data.products;
+}
+fetchProducts().then(() => {
+  loading.value = false;
+});
 </script>
 
 <template>
