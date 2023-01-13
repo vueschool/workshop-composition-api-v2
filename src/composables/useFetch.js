1f7a6ed1a23e33777654e0a7d181c26663076693
Daniel Kelly <me@danielkelly.io>
2023-01-13 10:52:13 -0600

    dont support reactive url yet

--- src/composables/useFetch.js
+++ src/composables/useFetch.js
@@ -1,25 +1,16 @@
-import { ref, isRef, watch, computed } from "vue";
+import { ref } from "vue";
 export const useFetch = (url) => {
-  // handle a reactive ref or a string
-  const URL = computed(() => {
-    return isRef(url) ? url.value : url;
-  });
   const loading = ref(true);
   const data = ref(null);
 
-  // wrap request in function to call right away
-  // and whenever URL changes
   function makeRequest() {
     loading.value = true;
-    fetch(URL.value).then(async (res) => {
+    fetch(url).then(async (res) => {
       data.value = await res.json();
       loading.value = false;
     });
   }
 
   makeRequest();
-
-  // watch URL for changes and make request
-  watch(URL, makeRequest);
   return { data, loading };
 };
